09044d11c5d677e542d65cc1ad80ebbba8776dd6
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2022-12-03 10:12:22 -0500

    End result

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,30 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     id("com.android.application") version "7.3.1" apply false
     id("com.android.library") version "7.3.1" apply false
+    id("com.github.ben-manes.versions") version "0.44.0"
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
 }
 
 tasks.create<Delete>("clean") {
     delete(rootProject.buildDir)
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+
+    checkForGradleUpdate = true
+    outputFormatter = "html"
+    outputDir = "build/dependencyUpdates"
+    reportfileName = "report"
 }
